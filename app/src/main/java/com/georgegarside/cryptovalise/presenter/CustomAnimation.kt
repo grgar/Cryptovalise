6cdc528a4e855cac57889d0b1b21442b56c41637
George Garside <grg.garside@gmail.com>
2018-04-02 18:14:01 +0100

    CustomAnimation timings & use val for fadeOut

--- app/src/main/java/com/georgegarside/cryptovalise/presenter/CustomAnimation.kt
+++ app/src/main/java/com/georgegarside/cryptovalise/presenter/CustomAnimation.kt
@@ -6,6 +6,9 @@ import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.georgegarside.cryptovalise.presenter.CustomAnimation.animateText
+import com.georgegarside.cryptovalise.presenter.CustomAnimation.fadeIn
+import com.georgegarside.cryptovalise.presenter.CustomAnimation.fadeOut
 
 /**
  * Custom animations used in the app for views, from simple [AlphaAnimation] of [fadeIn] or [fadeOut], to providing
@@ -13,23 +16,19 @@ import android.widget.TextView
  */
 object CustomAnimation {
 	/**
-	 * A basic fade in [AlphaAnimation] lasting 0.3s, with the object visible once complete
+	 * A basic fade in [AlphaAnimation]
 	 */
-	val fadeIn = AlphaAnimation(0.0f, 1.0f)
+	val fadeIn = AlphaAnimation(0f, 1f)
 	
 	/**
-	 * A basic fade out [AlphaAnimation] lasting 0.5s, with the object invisible once complete
+	 * A basic fade out [AlphaAnimation]
 	 */
-	private val fadeOut = AlphaAnimation(1.0f, 0.0f)
+	internal val fadeOut = AlphaAnimation(1f, 0f)
 	
 	init {
-		fadeIn.apply {
-			duration = 300
-			fillAfter = true
-		}
-		fadeOut.apply {
-			duration = 500
-			fillAfter = true
+		arrayOf(fadeIn, fadeOut).forEach {
+			it.duration = 500
+			it.fillAfter = true
 		}
 	}
 	
@@ -41,26 +40,32 @@ object CustomAnimation {
 	internal fun animateText(textView: TextView, animation: Animation, string: String, additionalView: View?) {
 		textView.apply {
 			text = string
-			alpha = 1.0f
+			alpha = 1f
 			this.animation = animation
 		}
 		additionalView?.apply {
-			alpha = 1.0f
+			alpha = 1f
 			this.animation = animation
 		}
 	}
 }
 
+/**
+ * [TextView.setText] of the [TextView] to the given [text] and [fadeIn] this text when changed.
+ * If an [additionalView] is given, [fadeIn] this [View] at the same time.
+ */
 fun TextView.fadeInText(text: String, additionalView: View? = null) =
 		CustomAnimation.animateText(this, CustomAnimation.fadeIn, text, additionalView)
 
 /**
  * Increase (or decrease) the [ProgressBar] progress by the [delta] given, with an [ObjectAnimator] animation.
- * If the progress bar is completed once the delta has been applied, the progress bar will [CustomAnimation.fadeOut].
+ * If the progress bar is completed (the [ProgressBar.getProgress] has reached [ProgressBar.getMax]) once the delta has
+ * been applied, the progress bar will [CustomAnimation.fadeOut].
  *
- * Reverse progress is supported through negative [delta] input. Once the progress bar reaches 100, the progress bar is
- * hidden. A previous revision of this function would show the progress bar again if necessary, however this causes
- * unwanted flashing in cases where the progress bar is reloaded at full completion, so this was deemed unnecessary.
+ * Reverse progress is supported through negative [delta] input. Once the progress bar reaches [ProgressBar.getMax],
+ * the progress bar is hidden. A previous revision of this function would show the progress bar again if necessary,
+ * however this causes unwanted flashing in cases where the progress bar is reloaded at completion, so this was deemed
+ * unnecessary.
  */
 fun ProgressBar.progressAnimate(delta: Int) {
 	// Calculate new percentage
@@ -68,12 +73,12 @@ fun ProgressBar.progressAnimate(delta: Int) {
 	
 	// Animate progress bar up to given progress
 	val animator = ObjectAnimator.ofInt(this, "progress", newProgress).apply {
-		duration = 600
+		duration = 800
 		start()
 	}
 	
 	// Fade out progress bar if completed
 	if (newProgress >= this.max) {
-		ObjectAnimator.ofFloat(this@progressAnimate, "alpha", 0.0f).setDuration(500).start()
+		this.animation = fadeOut
 	}
 }
