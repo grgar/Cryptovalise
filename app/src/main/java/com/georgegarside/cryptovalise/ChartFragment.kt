ad2461de81af22666acec222c18365a1107018b3
George Garside <grg.garside@gmail.com>
2018-04-04 16:48:15 +0100

    Add MPAndroidChart dependency

---
+++ app/src/main/java/com/georgegarside/cryptovalise/ChartFragment.kt
@@ -0,0 +1,49 @@
+package com.georgegarside.cryptovalise
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ChartFragment : Fragment() {
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		arguments?.let {
+/*
+			param1 = it.getString(ARG_PARAM1)
+			param2 = it.getString(ARG_PARAM2)
+*/
+		}
+	}
+	
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+	                          savedInstanceState: Bundle?): View? {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_chart, container, false)
+	}
+	
+	companion object {
+		/**
+		 * Use this factory method to create a new instance of
+		 * this fragment using the provided parameters.
+		 *
+		 * @param param1 Parameter 1.
+		 * @param param2 Parameter 2.
+		 * @return A new instance of fragment ChartFragment.
+		 */
+		// TODO: Rename and change types and number of parameters
+		fun newInstance(param1: String, param2: String) =
+				ChartFragment().apply {
+					arguments = Bundle().apply {
+/*
+						putString(ARG_PARAM1, param1)
+						putString(ARG_PARAM2, param2)
+*/
+					}
+				}
+	}
+}
