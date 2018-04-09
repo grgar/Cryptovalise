92e30ce01c81fd9c76c80c6988cbd767649638fb
George Garside <grg.garside@gmail.com>
2018-04-09 16:20:16 +0100

    CoinDetail links layout

--- app/src/main/java/com/georgegarside/cryptovalise/CoinDetailFragment.kt
+++ app/src/main/java/com/georgegarside/cryptovalise/CoinDetailFragment.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -48,57 +49,68 @@ class CoinDetailFragment : Fragment() {
 		
 		activity?.collapsingToolbar?.title = coin.name
 		
-		// Load prices
-		CoinRecyclerViewAdapter.loadPrices(view, symbol)
-		
-		// Coin description abbreviated
-		val shortDescription = coin.description
-				?.split(" ")
-				?.joinToString(
-						separator = " ",
-						prefix = "‘",
-						postfix = "’",
-						limit = 20,
-						truncated = "…"
-				)
-				?: getString(R.string.coin_detail_missing_description)
-		view.coinDescription.text = shortDescription
-		
-		// Coin logo
-		launch(UI) {
-			val logo = coin.logo.await() ?: return@launch
-			view.coinLogo.setImageBitmap(logo)
+		with(view) {
+			
+			// Load prices
+			CoinRecyclerViewAdapter.loadPrices(this, symbol)
+			
+			// Coin description abbreviated
+			val shortDescription = coin.description
+					?.split(" ")
+					?.joinToString(
+							separator = " ",
+							prefix = "‘",
+							postfix = "’",
+							limit = 20,
+							truncated = "…"
+					)
+					?: getString(R.string.coin_detail_missing_description)
+			coinDescription.text = shortDescription
+			coinDescriptionMore.setOnClickListener(showDescription(coin))
+			
+			// Coin logo asynchronous
+			launch(UI) {
+				val logo = coin.logo.await() ?: return@launch
+				coinLogo.setImageBitmap(logo)
+				coinLogoCopy.setOnClickListener(copyLogo(coin))
+			}
+			
+			// Coin market cap
+			capDelta.text = coin.delta.cap.first.format(NumberFormat.Delta, "%")
+			capDelta.setDeltaColour()
+			capTotal.text = coin.delta.cap.second.format()
+			
+			// Coin volume
+			volDelta.text = coin.delta.vol.first.format(NumberFormat.Delta, "%")
+			volDelta.setDeltaColour()
+			volTotal.text = coin.delta.vol.second.format()
+			
+			// Coin supply
+			supply.text = getString(R.string.coin_detail_supply, coin.supply.format())
+			if (coin.total == 0L) {
+				supplyTotal.text = getString(R.string.coin_detail_supply_total_unlimited)
+			} else {
+				supplyTotal.text = getString(R.string.coin_detail_supply_total, coin.total.format())
+			}
+			
+			// Coin dominance
+			domDelta.text = coin.delta.dom.first.format(NumberFormat.Delta, "%")
+			domDelta.setDeltaColour()
+			rank.text = getString(R.string.coin_detail_rank, coin.delta.dom.second)
 		}
-		
-		view.coinLogoCopy.setOnClickListener(copyLogo(coin))
-		
-		// Coin market cap
-		view.capDelta.text = coin.delta.cap.first.format(NumberFormat.Delta, "%")
-		view.capDelta.setDeltaColour()
-		view.capTotal.text = coin.delta.cap.second.format()
-		
-		// Coin volume
-		view.volDelta.text = coin.delta.vol.first.format(NumberFormat.Delta, "%")
-		view.volDelta.setDeltaColour()
-		view.volTotal.text = coin.delta.vol.second.format()
-		
-		// Coin supply
-		view.supply.text = getString(R.string.coin_detail_supply, coin.supply.format())
-		if (coin.total == 0L) {
-			view.supplyTotal.text = getString(R.string.coin_detail_supply_total_unlimited)
-		} else {
-			view.supplyTotal.text = getString(R.string.coin_detail_supply_total, coin.total.format())
-		}
-		
-		// Coin dominance
-		view.domDelta.text = coin.delta.dom.first.format(NumberFormat.Delta, "%")
-		view.domDelta.setDeltaColour()
-		view.rank.text = getString(R.string.coin_detail_rank, coin.delta.dom.second)
 	}
 	
 	private fun copyLogo(coin: API.Coin): (View) -> Unit = {
 		val clipboard = context?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 		val clipUri = ClipData.newRawUri("Logo for ${coin.name}", Uri.parse(coin.logoPath))
 		clipboard.primaryClip = clipUri
+	}
+	
+	private fun showDescription(coin: API.Coin): (View) -> Unit = { view ->
+		AlertDialog.Builder(view.context).apply {
+			setMessage(coin.description?.replace(". ", ".\n\n"))
+			setNeutralButton(getString(R.string.close), { dialog, _ -> dialog.dismiss() })
+			show()
+		}
 	}
 }
