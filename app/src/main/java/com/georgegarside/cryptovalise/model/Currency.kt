48736410e437960fe07ca0396a33ec390b2730b2
George Garside <grg.garside@gmail.com>
2018-04-11 14:14:30 +0100

    Documenting Currency

--- app/src/main/java/com/georgegarside/cryptovalise/model/Currency.kt
+++ app/src/main/java/com/georgegarside/cryptovalise/model/Currency.kt
@@ -1,3 +1,21 @@
 package com.georgegarside.cryptovalise.model
 
-data class Currency(val code: String = "", val name: String = "", val rate: Double = 0.0)
+/**
+ * A currency from the [API]. Each currency has a currency [code] which is the standard code used to represent the
+ * currency internationally (this is unique for each currency), a [name] in English for user-identification of the
+ * currency, and the exchange [rate] of the currency with USD.
+ */
+data class Currency(
+		/**
+		 * The international standard currency code for this currency.
+		 */
+		val code: String = "",
+		/**
+		 * The English name for the currency.
+		 */
+		val name: String = "",
+		/**
+		 * The exchange rate of the currency with USD.
+		 */
+		val rate: Double = 0.0
+)
